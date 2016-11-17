ec15478b26281a59425358cbec56db48f11287e4
Adam Gorbahn and Andrew Fisher <adam+andrew@>
2016-11-17 08:59:44 -0800

    user click on specific item in a listview

--- app/src/main/java/com/epicodus/localbuisness/MainActivity.java
+++ app/src/main/java/com/epicodus/localbuisness/MainActivity.java
@@ -2,9 +2,13 @@ package com.epicodus.localbuisness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -31,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, mMajor);
         mMajorListView.setAdapter(adapter);
+        mMajorListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(MainActivity.this, mMajor[position], Toast.LENGTH_SHORT).show();
 
+            }
+        });
     }
 }
